8e5bb072b578c00c8693d379ab5b28dab2abd976
jordiugarte <jordi933@gmail.com>
2020-11-04 10:32:55 -0400

    refresh

--- app/src/main/java/com/asiesinc/appredes/MainActivity.java
+++ app/src/main/java/com/asiesinc/appredes/MainActivity.java
@@ -100,4 +100,8 @@ public class MainActivity extends AppCompatActivity {
         RequestQueue requestQueue = Volley.newRequestQueue(this);
         requestQueue.add(stringRequest);
     }
+
+    public void refresh(View view){
+        update();
+    }
 }
